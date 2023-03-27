6b520ad1e8fbb81ab446e38a1aee76f3d7bb08f5
Amiel Brencis Salipande <absalipande@gmail.com>
2023-03-27 20:22:39 +0800

    fix: small changes

--- src/lib/openai.ts
+++ src/lib/openai.ts
@@ -1,5 +1,4 @@
-import { Configuration } from "openai";
-import { OpenAIApi } from "openai/dist/api";
+import { Configuration, OpenAIApi } from "openai";
 
 const configuration = new Configuration({
     apiKey: process.env.OPENAI_API_KEY
